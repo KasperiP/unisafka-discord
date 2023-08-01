e0d9e7c6702e0da8868ec524764c3c4e42f46576
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2023-08-01 18:53:39 +0300

    update menu logic

--- src/commands/ruokalista.js
+++ src/commands/ruokalista.js
@@ -1,10 +1,34 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { fetchMenu } = require('../functions/fetchMenu');
+const { getNextDateOfTheDay } = require('../functions/getNextDate');
+
+const weekDaysShort = ['su', 'ma', 'ti', 'ke', 'to', 'pe', 'la'];
+const weekDays = [
+  'Sunnuntai',
+  'Maanantai',
+  'Tiistai',
+  'Keskiviikko',
+  'Torstai',
+  'Perjantai',
+  'Lauantai',
+];
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ruokalista')
     .setDescription('Palauttaa päivän ruokalistan.')
+    .addStringOption((option) =>
+      option
+        .setName('kampus')
+        .setDescription('Valitse kampus, jonka ravintolat haluat nähdä.')
+        .setRequired(true)
+        .addChoices(
+          { name: 'Hervannan kampus', value: 'tty' },
+          { name: 'Kaupin kampus', value: 'tays' },
+          { name: 'Keskustakampus', value: 'tay' },
+          { name: 'Tampereen ammattikorkeakoulu', value: 'tamk' },
+        ),
+    )
     .addStringOption((option) =>
       option
         .setName('päivä')
@@ -21,67 +45,80 @@ module.exports = {
     ),
 
   async execute(interaction) {
-    const day = interaction.options.getString('päivä');
+    const campus = interaction.options.getString('kampus');
+    const day =
+      interaction.options.getString('päivä') ||
+      weekDaysShort[new Date().getDay()];
 
-    const menu = (await fetchMenu(day)).restaurants_tay;
-    const weekDaysShort = ['su', 'ma', 'ti', 'ke', 'to', 'pe', 'la'];
-    const weekDays = [
-      'Sunnuntai',
-      'Maanantai',
-      'Tiistai',
-      'Keskiviikko',
-      'Torstai',
-      'Perjantai',
-      'Lauantai',
-    ];
-    const wantedRestaurants = ['res_yr', 'res_linna', 'res_minerva'];
+    const menu = await fetchMenu(day);
+    const wantedRestaurants = menu?.restaurants?.[campus];
+    const availableMeals = menu?.[`restaurants_${campus}`];
 
-    const outputs = new Object();
+    if (!wantedRestaurants || !availableMeals) {
+      return interaction.reply('Ruokalistoja ei saatavilla.');
+    }
+
+    if (!campus) {
+      return interaction.reply('Valitse kampus.');
+    }
+
+    if (
+      weekDaysShort.indexOf(day) < new Date().getDay() &&
+      weekDaysShort.indexOf(day) !== 0
+    ) {
+      return interaction.reply(
+        `Päivä ${
+          weekDays[weekDaysShort.indexOf(day)]
+        } on jo mennyt, eikä seuraavan viikon ruokalistoja ole vielä saatavilla! 😢`,
+      );
+    }
+
+    const outputs = {};
 
     wantedRestaurants.forEach((restaurant) => {
-      const resMenu = menu[restaurant];
-      outputs[resMenu.restaurant] = '';
-
-      if (resMenu.meals.length === 0) {
-        outputs[resMenu.restaurant] = 'Ravintola kiinni';
-      } else {
-        const mo = resMenu.meals.map((meal) => meal.mo);
-        mo.forEach((meal, index) => {
-          const foodObj = meal.map((food) => food.mpn);
-          outputs[resMenu.restaurant] += `**${index + 1})** ${foodObj.join(
-            ', ',
-          )}\n`;
-        });
-      }
+      const resMenu = availableMeals?.[restaurant];
+      if (!resMenu) return;
+      outputs[restaurant] =
+        resMenu.meals.length === 0
+          ? 'Ravintola kiinni'
+          : resMenu.meals
+              .map((meal, index) => {
+                const foodObj = meal.mo
+                  .map((menuItem) => menuItem.mpn.replace(/[^a-zåäö\s]/gi, ''))
+                  .join(', ');
+                return `*(${index + 1})* ${foodObj}\n`;
+              })
+              .join('');
     });
 
-    let dateStr = '';
-    if (day) {
-      dateStr += `${weekDays[weekDaysShort.indexOf(day)]} ${
-        new Date().getDate() + weekDaysShort.indexOf(day) - new Date().getDay()
-      }.${new Date().getMonth() + 1}.`;
-    } else {
-      dateStr = `${weekDays[new Date().getDay()]} ${new Date().getDate()}.${
-        new Date().getMonth() + 1
-      }.`;
-    }
+    const dateStr = `${
+      weekDays[weekDaysShort.indexOf(day)]
+    } ${getNextDateOfTheDay(day)}.`;
 
     const embed = new EmbedBuilder();
     embed.setTitle(`Ruokalistat ${dateStr}`);
-    embed.setColor('#0099ff');
+    embed.setColor('#9867fb');
 
     Object.entries(outputs).forEach(([res, list]) => {
-      embed.addFields({ name: res, value: list });
+      embed.addFields({
+        name: `*${availableMeals[res].restaurant}*${
+          availableMeals[res].eating_hours !== '' &&
+          availableMeals[res].meals.length !== 0
+            ? ` *(${availableMeals[res].eating_hours})*`
+            : ''
+        }`,
+        value: list,
+      });
     });
 
     embed.setAuthor({
       name: 'Unisafka',
-      iconURL: 'https://unisafka.fi/static/logo.png',
+      iconURL: 'https://i.imgur.com/FckqkLu.png',
       url: 'https://unisafka.fi/',
     });
     embed.setFooter({
       text: 'Unisafka',
-      iconURL: 'https://unisafka.fi/static/logo.png',
+      iconURL: 'https://i.imgur.com/FckqkLu.png',
     });
     embed.setTimestamp();
 
